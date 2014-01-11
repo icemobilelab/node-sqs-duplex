e922603cb0f95d6e0a380d65bc69ab2b54c8e976
Brian M. Carlson <brian.m.carlson@gmail.com>
2014-01-11 17:56:28 -0600

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,67 @@
+var assert = require('assert')
+var AWS = require('aws-sdk')
+var aws = new AWS.SQS()
+var sqs = require('../')(aws)
+
+var create = function(name, cb) {
+  var options = {
+    QueueName: name
+  }
+  aws.createQueue(options, cb)
+}
+
+var queueTest = function() {
+  before(function(done) {
+    var name = this.name = 'test-' + Date.now()
+    this.stream = sqs(name, {log: console.log.bind(console)})
+    var self = this
+    create(name, function(err, data) {
+      self.queueUrl = data.QueueUrl
+      done()
+    })
+  })
+
+  after(function(done) {
+    aws.deleteQueue({QueueUrl: this.queueUrl}, done)
+  })
+}
+
+describe('writing', function() {
+  queueTest()
+
+  it('writes', function(done) {
+    this.timeout(5000)
+    this.stream.write('hello', done)
+  })
+})
+
+describe('reading', function() {
+  queueTest()
+  it('reads', function(done) {
+    var stream = this.stream
+    stream.write('test', function() {
+      stream.once('readable', function() {
+        var msg = stream.read()
+        assert(msg, 'should have returned a message')
+        assert.equal(msg.data, 'test')
+        stream.close()
+        done()
+      })
+    })
+  })
+})
+
+describe('reading empty', function() {
+  queueTest()
+  it('never returns anything', function(done) {
+    this.timeout(20000)
+    this.stream.once('readable', function() {
+      done(new Error('Should not have become readable'))
+    })
+    var self = this
+    setTimeout(function() {
+      self.stream.close()
+      done()
+    }, 5000)
+  })
+})
